4fd5146d245413704f0f4154ea2c86159eefbde7
ovie009 <ovieiffieu2much@gmail.com>
2021-12-24 21:00:28 +0100

    download canvas

--- canvas.js
+++ canvas.js
@@ -8,17 +8,12 @@ let oy = canvas.height/2;
 ctx.font = "42px serif";
 ctx.textAlign = "center";
 ctx.textBaseline = "middle";
-ctx.fillStyle = "#FFF";
-ctx.fillText("Hello World!", ox, oy);
+ctx.fillStyle = "#800";
+ctx.fillRect(ox/2, oy/2, ox, oy);
 
 
-let rotateCtx = function () {
-    // tramslate so that the origin in mow (ox, oy) the center of the canvas
-    ctx.translate(ox, oy);
-    // convert degrees to radians with radians = (Math.PI/180)*degrees
-    ctx.rotate((Math.PI/180)*15);
-    ctx.fillText("Hello World!", 0, 0);
-    // translate back
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.translate(-ox, -oy);
+let downloadImg = function (el) {
+    // get image URI from canvas object
+    let imageURI = canvas.toDataURL("image/jpg");
+    el.href = imageURI;
 }
