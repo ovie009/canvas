2a1c55bc97d37ad66b73547399017e55f0e1c64f
ovie009 <ovieiffieu2much@gmail.com>
2021-12-24 20:45:07 +0100

    Rotate the canvas

--- canvas.js
+++ canvas.js
@@ -2,16 +2,23 @@
 // Getting Started
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext("2d");
-ctx.font = "16px Arial";
+let ox =  canvas.width/2;
+let oy = canvas.height/2;
+
+ctx.font = "42px serif";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.fillStyle = "#FFF";
+ctx.fillText("Hello World!", ox, oy);
 
 
-canvas.addEventListener("mousemove", function(e) {
-    
-    // console.log("Hello World!");
-    let cRect = canvas.getBoundingClientRect();  //gets CSS pos, and width/height
-    let canvasX = Math.round(e.x - cRect.left); // substarct the 'left' of the canvas
-    let canvasY = Math.round(e.y - cRect.top); // from the X/Y postion to make
-    ctx.clearRect(0, 0, canvas.width, canvas.height);   // (0, 0) the top left of the canvas
-    ctx.fillText(`X: ${canvasX}, Y: ${canvasY}`, 10, 20);
-    
-})
+let rotateCtx = function () {
+    // tramslate so that the origin in mow (ox, oy) the center of the canvas
+    ctx.translate(ox, oy);
+    // convert degrees to radians with radians = (Math.PI/180)*degrees
+    ctx.rotate((Math.PI/180)*15);
+    ctx.fillText("Hello World!", 0, 0);
+    // translate back
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.translate(-ox, -oy);
+}
