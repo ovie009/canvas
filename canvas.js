d7ad58fc30fa6087e3772ed5ca383e633c9be10d
ovie009 <ovieiffieu2much@gmail.com>
2021-12-27 16:15:10 +0100

    wrap Text

--- canvas.js
+++ canvas.js
@@ -4,128 +4,33 @@
 // render text along a curve
 // pass 8 values for cubic bezier
 // pass 6 values for quadratic
-// Renders text from start of curve 
-var textOnCurve = function(text,offset,x1,y1,x2,y2,x3,y3,x4,y4){
-    ctx.save();
-    ctx.textAlign = "center";
-    var widths = [];
-    for(var i = 0; i < text.length; i ++){
-        widths[widths.length] = ctx.measureText(text[i]).width;
-    }
-    var ch = curveHelper(x1,y1,x2,y2,x3,y3,x4,y4);
-    var pos = offset;
-    var cpos = 0;
+// wrap text into paragraphs
+function wrapText(text, x, y, maxWidth, fontSize, fontFace){
+    var firstY=y;
+    var words = text.split(' ');
+    var line = '';  var lineHeight=fontSize*1.286; // a good approx for 10-18px sizes
 
-    for(var i = 0; i < text.length; i ++){
-        pos += widths[i] / 2;
-        cpos = ch.forward(pos);
-        ch.tangent(cpos);
-        ctx.setTransform(ch.vect.x, ch.vect.y, -ch.vect.y, ch.vect.x, ch.vec.x, ch.vec.y);
-        ctx.fillText(text[i],0,0);
-
-        pos += widths[i] / 2;
-    }    
-    ctx.restore(); 
-}
-
-// helper function locates points on bezier curves. 
-function curveHelper(x1, y1, x2, y2, x3, y3, x4, y4){
-    var tx1, ty1, tx2, ty2, tx3, ty3, tx4, ty4;
-    var a,b,c,u;
-    var vec,currentPos,vec1,vect;
-    vec = {x:0,y:0};
-    vec1 = {x:0,y:0};
-    vect = {x:0,y:0};
-    quad = false;
-    currentPos = 0;
-    currentDist = 0;
-    if(x4 === undefined || x4 === null){
-        quad = true;
-        x4 = x3;
-        y4 = y3;
-    }
-    var estLen = Math.sqrt((x4 - x1) * (x4 - x1) + (y4 - y1) * (y4 - y1));
-    var onePix = 1 / estLen;
-    function posAtC(c){
-        tx1 = x1; ty1 = y1;
-        tx2 = x2; ty2 = y2;
-        tx3 = x3; ty3 = y3;
-        tx1 += (tx2 - tx1) * c;
-        ty1 += (ty2 - ty1) * c;
-        tx2 += (tx3 - tx2) * c;
-        ty2 += (ty3 - ty2) * c;
-        tx3 += (x4 - tx3) * c;
-        ty3 += (y4 - ty3) * c;
-        tx1 += (tx2 - tx1) * c;
-        ty1 += (ty2 - ty1) * c;
-        tx2 += (tx3 - tx2) * c;
-        ty2 += (ty3 - ty2) * c;
-        vec.x = tx1 + (tx2 - tx1) * c;
-        vec.y = ty1 + (ty2 - ty1) * c;
-        return vec;
-    }    
-    function posAtQ(c){
-        tx1 = x1; ty1 = y1;
-        tx2 = x2; ty2 = y2;
-        tx1 += (tx2 - tx1) * c;
-        ty1 += (ty2 - ty1) * c;
-        tx2 += (x3 - tx2) * c;
-        ty2 += (y3 - ty2) * c;
-        vec.x = tx1 + (tx2 - tx1) * c;
-        vec.y = ty1 + (ty2 - ty1) * c;
-        return vec;
-    }
-    function forward(dist){
-        var step;
-        helper.posAt(currentPos);
-
-        while(currentDist < dist){
-            vec1.x = vec.x;
-            vec1.y = vec.y;
-            currentPos += onePix;
-            helper.posAt(currentPos);
-            currentDist += step = Math.sqrt((vec.x - vec1.x) * (vec.x - vec1.x) + (vec.y - vec1.y) * (vec.y - vec1.y));
+    ctx.font=fontSize+" "+fontFace;
+    ctx.textBaseline='top';
+  
+    for(var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + ' ';
+        var metrics = ctx.measureText(testLine);
+        var testWidth = metrics.width;
+        if(testWidth > maxWidth) {
+            ctx.fillText(line, x, y);
+            if(n<words.length-1){
+                line = words[n] + ' ';
+                y += lineHeight;
+            }
         }
-        currentPos -= ((currentDist - dist) / step) * onePix;
-        currentDist -= step;
-        helper.posAt(currentPos);
-        currentDist += Math.sqrt((vec.x - vec1.x) * (vec.x - vec1.x) + (vec.y - vec1.y) * (vec.y - vec1.y));
-        return currentPos;
+        else {
+            line = testLine;
+        }
     }
-    function tangentQ(pos){
-        a = (1-pos) * 2;
-        b = pos * 2;
-        vect.x = a * (x2 - x1) + b * (x3 - x2);
-        vect.y = a * (y2 - y1) + b * (y3 - y2);
-        u = Math.sqrt(vect.x * vect.x + vect.y * vect.y);
-        vect.x /= u;
-        vect.y /= u;
-    }
-    function tangentC(pos){
-        a  = (1-pos);
-        b  = 6 * a * pos;
-        a *= 3 * a;
-        c  = 3 * pos * pos;
-        vect.x  = -x1 * a + x2 * (a - b) + x3 * (b - c) + x4 * c;
-        vect.y  = -y1 * a + y2 * (a - b) + y3 * (b - c) + y4 * c;
-        u = Math.sqrt(vect.x * vect.x + vect.y * vect.y);
-        vect.x /= u;
-        vect.y /= u;
-    }
-    var helper = {
-        vec : vec,
-        vect : vect,
-        forward : forward,
-    }
-    if(quad){
-        helper.posAt = posAtQ;
-        helper.tangent = tangentQ;
-    }else{
-        helper.posAt = posAtC;
-        helper.tangent = tangentC;
-    }
-    return helper;
+    ctx.fillText(line, x, y);
 } 
+
 
 //canvas variable
 let canvas = document.getElementById("myCanvas");
@@ -148,6 +53,10 @@ function randomFloat(min, max) {
 //text
 let text = 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet';
 ctx.font = "20px arial";
+let x= 10;
+let y = 10;
+let maxWidth = 250;
+let fontSize = 20;
+let fontFace = 'arial';
 
-textOnCurve(text, 10, 100, 500, 300, 450, 250, 200);
-curveHelper(100, 500, 300, 450, 250, 200);
+wrapText(text, x, y, maxWidth, fontSize, fontFace);
