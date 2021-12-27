504578d669e046284fdfd01a827214285810d930
ovie009 <ovieiffieu2much@gmail.com>
2021-12-27 19:01:30 +0100

    fill image into text

--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
-//chapter 1 
-// Getting Started
+//chapter 2
+// Text
 
 //canvas variable
 let canvas = document.getElementById("myCanvas");
@@ -20,53 +20,28 @@ function randomFloat(min, max) {
 }
 
 //text
-let text = 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet';
-ctx.font = "20px arial";
-let x= 10;
-let y = 10;
+let text = 'Lorem ipsum.';
+let font = "50px arial";
+let x= 30;
+let y = canvas.height/2;
 let maxWidth = 250;
 let fontSize = 20;
 let fontFace = 'arial';
+// let img = document.getElementById("img");
+// let img = ;
+let img = document.getElementById("myImg");
 
-// ctx.scale(3, 3);
-ctx.beginPath();
-ctx.moveTo(75,40);
-ctx.bezierCurveTo(75,37,70,25,50,25);
-ctx.bezierCurveTo(20,25,20,62.5,20,62.5);
-ctx.bezierCurveTo(20,80,40,102,75,120);
-ctx.bezierCurveTo(110,102,130,80,130,62.5);
-ctx.bezierCurveTo(130,62.5,130,25,100,25);
-ctx.bezierCurveTo(85,25,75,37,75,40);
-ctx.fillStyle='red';
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-// ctx.setTransform(1,0,0,1,0,0);
-ctx.fillStyle='blue';
-ctx.arc(75, 40, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-ctx.arc(50, 25, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-ctx.arc(20, 62.5, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-ctx.arc(75, 120, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-ctx.arc(130, 62.5, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-ctx.arc(100, 25, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
-ctx.beginPath()
-ctx.arc(75, 40, 5, 0, 2*Math.PI, false);
-ctx.fill();
-ctx.closePath();
+
+function drawImageInsideText(canvas,x,y,img,text,font){
+    var c=canvas.cloneNode();
+    var ctx=c.getContext('2d');
+    ctx.font=font;
+    ctx.fillText(text,x,y);
+    ctx.globalCompositeOperation='source-atop';
+    ctx.drawImage(img,0,0);
+    canvas.getContext('2d').drawImage(c,0,0);
+}
+
+
+drawImageInsideText(canvas, x, y, img, text, font);
+
