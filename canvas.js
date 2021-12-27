c2cdc3ff2f166cee32a0adbf9410c0e948ce06d5
ovie009 <ovieiffieu2much@gmail.com>
2021-12-27 19:29:08 +0100

    fill text with image, image variable fixed

--- canvas.js
+++ canvas.js
@@ -4,10 +4,10 @@
 //canvas variable
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth - 20;
-canvas.height = window.innerHeight - 20;
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
+// canvas.width = 500;
+// canvas.height = 500;
 
 //random integer
 function randomInteger(min, max) {
@@ -21,27 +21,29 @@ function randomFloat(min, max) {
 
 //text
 let text = 'Lorem ipsum.';
-let font = "50px arial";
+let font = "130px arial";
 let x= 30;
 let y = canvas.height/2;
 let maxWidth = 250;
 let fontSize = 20;
 let fontFace = 'arial';
-// let img = document.getElementById("img");
-// let img = ;
-let img = document.getElementById("myImg");
+
+let img = new Image();
+img.src = "xidingart-20200413-0001.jpg";
+img.onerror = function(){alert(img.src+' failed');}
+// img.src = "/xidingart-20200413-0001.jpg";
 
 
 function drawImageInsideText(canvas,x,y,img,text,font){
-    var c=canvas.cloneNode();
-    var ctx=c.getContext('2d');
-    ctx.font=font;
+    var c = canvas.cloneNode();
+    var ctx = c.getContext('2d');
+    ctx.font = font;
     ctx.fillText(text,x,y);
-    ctx.globalCompositeOperation='source-atop';
+    ctx.globalCompositeOperation = 'source-atop';
     ctx.drawImage(img,0,0);
     canvas.getContext('2d').drawImage(c,0,0);
 }
 
+img.onload = drawImageInsideText(canvas, x, y, img, text, font);
 
-drawImageInsideText(canvas, x, y, img, text, font);
 
