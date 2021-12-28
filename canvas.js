09b1347dd5376acca0be3aad5203ced10c6321a1
ovie009 <ovieiffieu2much@gmail.com>
2021-12-28 20:54:54 +0100

    draw multiple images on the canvas

--- canvas.js
+++ canvas.js
@@ -27,16 +27,31 @@ let colorArray = [
     "#d94c1a",
 ]
 
-// check if image is loaded
-let img = new Image();
-img.onload = start();
-img.onerror = function () {
-    alert(img.src+' failed');
+// load image 1
+let img1 = new Image();
+img1.onload = start;
+img1.onerror = function () {
+    alert(img1.src+' failed');
 }
-img.src = "xidingart-20200413-0001.jpg";
+img1.src = "xidingart-20200413-0001.jpg";
 
+// load image 2
+let img2 = new Image();
+img2.onload = start;
+img2.onerror = function () {
+    alert(img2.src+' failed');
+}
+img2.src = "comics_hall-20200413-0007.jpg";
+let imgCount = 2;
+
+// start is called everytime an image loads
 function start() {
-    //start is called after the image is fully loaded
-    // regardless of start's position in the code
-    console.log('image loaded successfully')
+    // countdown until all images are loaded
+    if (--imgCount > 0) {
+        return;
+    }
+    // all the images are now successfully loaded
+    // context.drawImage will successfully draw each one
+    ctx.drawImage(img1, 0, 0);
+    ctx.drawImage(img2, 0, canvas.height/2);
 }
