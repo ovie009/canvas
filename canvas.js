5cc3309c11e34ab3c1a35edf68aba7d3adb67639
ovie009 <ovieiffieu2much@gmail.com>
2021-12-29 12:59:39 +0100

    scale image to fill or fit the canvas

--- canvas.js
+++ canvas.js
@@ -4,6 +4,10 @@
 //canvas variable
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+let canvas2 = document.getElementById("canvasFill");
+let ctx2 = canvas2.getContext("2d");
+let canvas3 = document.getElementById("canvasFit");
+let ctx3 = canvas3.getContext("2d");
 canvas.width = window.innerWidth - 10;
 canvas.height = window.innerHeight - 10;
 canvas.width = 850;
@@ -27,26 +31,36 @@ let colorArray = [
     "#d94c1a",
 ]
 
-function cropImage(image, croppingCoords) {
-    var cc = croppingCoords;
-    var workCan = document.createElement("canvas"); // create a canvas
-    workCan.width = Math.floor(cc.width);  // set the canvas resolution to the cropped image size
-    workCan.height = Math.floor(cc.height);
-    var ctx = workCan.getContext("2d");    // get a 2D rendering interface    
-    ctx.drawImage(image, -Math.floor(cc.x), -Math.floor(cc.y)); // draw the image offset to place it correctly on the cropped region
-    image.src = workCan.toDataURL();       // set the image source to the canvas as a data URL
-    return image; 
+//scale to fit
+function scaleToFit(img, canvas, ctx){
+    // get the scale
+    var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+    // get the top left position of the image
+    var x = (canvas.width / 2) - (img.width / 2) * scale;
+    var y = (canvas.height / 2) - (img.height / 2) * scale;
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
 }
+
+//scale to fill
+function scaleToFill(img, canvas, ctx){
+    // get the scale
+    var scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+    // get the top left position of the image
+    var x = (canvas.width / 2) - (img.width / 2) * scale;
+    var y = (canvas.height / 2) - (img.height / 2) * scale;
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+}
+
+// load image 1
+let img1 = new Image();
+img1.src = "xidingart-20200413-0001.jpg"; //load image
+img1.onload = function () { //when loaded
+    scaleToFill(this, canvas2, ctx2)
+};
 
 // load image 2
 let img2 = new Image();
-img2.src = "comics_hall-20200413-0007.jpg"; //load image
+img2.src = "texte-20210420-0001.jpg"; //load image
 img2.onload = function () { //when loaded
-    cropImage(this, {
-        x: this.width/4, //crop keeping the center
-        y: this.height/4, 
-        width: this.width/2, 
-        height: this.height/2, 
-    });    
-    document.body.appendChild(this); //add the image to the DOM
+    scaleToFill(this, canvas, ctx)
 };
