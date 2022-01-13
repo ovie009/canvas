4e311523ae2a147d96da4c5902b825fc9e99d7ed
ovie009 <ovieiffieu2much@gmail.com>
2022-01-13 13:42:07 +0100

    Bresenham's Line (line without blurryness)

--- canvas.js
+++ canvas.js
@@ -26,10 +26,49 @@ let colorArray = [
     "#57523e",
     "#d94c1a",
 ]
+// x,y line start
+// xx,yy line end
+// the pixel at line start and line end are drawn
+function bresenhamLine(x, y, xx, yy){
+    var oldFill = ctx.fillStyle; // save old fill style
+    ctx.fillStyle = ctx.strokeStyle; // move stroke style to fill
+    xx = Math.floor(xx);
+    yy = Math.floor(yy);
+    x = Math.floor(x);
+    y = Math.floor(y);
+    // BRENSENHAM
+    var dx = Math.abs(xx-x);
+    var sx = x < xx ? 1 : -1;
+    var dy = -Math.abs(yy-y);
+    var sy = y<yy ? 1 : -1;
+    var err = dx+dy;
+    var errC; // error value
+    var end = false;
+    var x1 = x;
+    var y1 = y;
+    while(!end){
+        ctx.fillRect(x1, y1, 1, 1); // draw each pixel as a rect
+        if (x1 === xx && y1 === yy) {
+            end = true;
+        }else{
+            errC = 2*err;
+            if (errC >= dy) {
+                err += dy;
+                x1 += sx;
+            }
+            if (errC <= dx) {
+                err += dx;
+                y1 += sy;
+            }
+        }
+    }
+    ctx.fillStyle = oldFill; // restore old fill style
+}
 
-ctx.fillStyle = 'tomato';
-ctx.strokeStyle = 'teal';
-ctx.lineWidth = 5;
-ctx.ellipse(canvas.width/2, canvas.height/2, 240, 150, Math.PI/4, 0, 2*Math.PI, false);
-ctx.fill();
+bresenhamLine(30, 30, 200, 200);
+
+ctx.lineWidth = 3;
+ctx.beginPath();
+ctx.moveTo(130, 30);
+ctx.lineTo(300, 200)
 ctx.stroke();
