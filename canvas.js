1ea895c0eea2af084f429f20e3382c320dae46bf
ovie009 <ovieiffieu2much@gmail.com>
2022-01-13 14:55:22 +0100

    Move Shape horizontally

--- canvas.js
+++ canvas.js
@@ -29,10 +29,33 @@ let colorArray = [
     "#d94c1a",
 ]
 
-ctx.beginPath();
-ctx.arc(centerX, centerY, 50, 0, 2*Math.PI);
-ctx.stroke();
-ctx.fillStyle = colorArray[randomInteger(0, 5)];
-// ctx.fillStyle = colorArray[4];
-ctx.fill();
-console.log("end of the line");
+// ctx.beginPath();
+// ctx.arc(centerX, centerY, 50, 0, 2*Math.PI);
+// ctx.stroke();
+// ctx.fillStyle = colorArray[randomInteger(0, 5)];
+// // ctx.fillStyle = colorArray[4];
+// ctx.fill();
+// console.log("end of the line");
+
+function animate() {
+    
+    // update the x position of the circle
+
+    centerX += 0.5; // the value represents how fast it moves
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 50, 0, 2*Math.PI);
+    ctx.fillStyle = colorArray[4];
+    ctx.fill();
+
+    // if the ball reaches the end of the canvas make reset its center to the left hand side
+    if (centerX > canvas.width + 110) {
+        centerX = -50;
+    }
+    
+    // request another animate() loop
+    requestAnimationFrame(animate);
+}
+
+animate();
