d67424b805410aa0bfbc61017e15bfa74d7e1a22
ovie009 <ovieiffieu2much@gmail.com>
2022-01-13 14:35:57 +0100

    Basic Static Circle

--- canvas.js
+++ canvas.js
@@ -4,10 +4,12 @@
 //canvas variable
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 10;
 // canvas.width = 500;
 // canvas.height = 500;
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
+let centerX = canvas.width/2;
+let centerY = canvas.height/2;
 
 //random integer
 function randomInteger(min, max) {
@@ -26,49 +28,11 @@ let colorArray = [
     "#57523e",
     "#d94c1a",
 ]
-// x,y line start
-// xx,yy line end
-// the pixel at line start and line end are drawn
-function bresenhamLine(x, y, xx, yy){
-    var oldFill = ctx.fillStyle; // save old fill style
-    ctx.fillStyle = ctx.strokeStyle; // move stroke style to fill
-    xx = Math.floor(xx);
-    yy = Math.floor(yy);
-    x = Math.floor(x);
-    y = Math.floor(y);
-    // BRENSENHAM
-    var dx = Math.abs(xx-x);
-    var sx = x < xx ? 1 : -1;
-    var dy = -Math.abs(yy-y);
-    var sy = y<yy ? 1 : -1;
-    var err = dx+dy;
-    var errC; // error value
-    var end = false;
-    var x1 = x;
-    var y1 = y;
-    while(!end){
-        ctx.fillRect(x1, y1, 1, 1); // draw each pixel as a rect
-        if (x1 === xx && y1 === yy) {
-            end = true;
-        }else{
-            errC = 2*err;
-            if (errC >= dy) {
-                err += dy;
-                x1 += sx;
-            }
-            if (errC <= dx) {
-                err += dx;
-                y1 += sy;
-            }
-        }
-    }
-    ctx.fillStyle = oldFill; // restore old fill style
-}
 
-bresenhamLine(30, 30, 200, 200);
-
-ctx.lineWidth = 3;
 ctx.beginPath();
-ctx.moveTo(130, 30);
-ctx.lineTo(300, 200)
+ctx.arc(centerX, centerY, 50, 0, 2*Math.PI);
 ctx.stroke();
+ctx.fillStyle = colorArray[randomInteger(0, 5)];
+// ctx.fillStyle = colorArray[4];
+ctx.fill();
+console.log("end of the line");
