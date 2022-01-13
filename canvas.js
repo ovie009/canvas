a6f1d7b7d2b798941c61f9e2fb7e862696f501df
ovie009 <ovieiffieu2much@gmail.com>
2022-01-13 13:24:38 +0100

    Paths, Ellipse

--- canvas.js
+++ canvas.js
@@ -27,68 +27,9 @@ let colorArray = [
     "#d94c1a",
 ]
 
-let linearGradient = ctx.createLinearGradient(300, 100, 450, 400);
-linearGradient.addColorStop(0, 'orange');
-linearGradient.addColorStop(1, 'teal');
-
-let radialGradient = ctx.createRadialGradient(450, 450, 25, 450, 450, 50);
-radialGradient.addColorStop(0, 'rebeccapurple');
-radialGradient.addColorStop(1, 'tomato');
-ctx.strokeStyle = linearGradient;
-
-ctx.beginPath();
-ctx.lineWidth = 9;
-ctx.lineCap = 'round'; // "butt" (default), 'square'
-//lineCap, styles the end of the line
-ctx.lineTo(200, 200);
-ctx.lineTo(300, 300);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.lineWidth = 9;
-ctx.lineCap = 'square'; // "butt" (default), 'square'
-//lineCap, styles the end of the line
-ctx.lineTo(300, 200);
-ctx.lineTo(400, 300);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.lineWidth = 9;
-ctx.lineCap = 'butt'; // "butt" (default), 'square'
-//lineCap, styles the end of the line
-ctx.lineTo(100, 200);
-ctx.lineTo(200, 300);
-ctx.stroke();
-
-
-ctx.beginPath();
-ctx.lineWidth = 9;
-ctx.lineJoin = 'round'; // "miter" (default), 'bevel'
-//lineCap, styles the end of the line
-ctx.lineTo(400, 200);
-ctx.lineTo(450, 250);
-ctx.lineTo(500, 200);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.lineWidth = 9;
-ctx.lineJoin = 'bevel'; // "miter" (default), 'bevel'
-//lineCap, styles the end of the line
-ctx.lineTo(550, 200);
-ctx.lineTo(600, 250);
-ctx.lineTo(650, 200);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.lineWidth = 9;
-ctx.lineJoin = 'miter'; // "miter" (default), 'bevel'
-//lineCap, styles the end of the line
-ctx.lineTo(700, 200);
-ctx.lineTo(750, 250);
-ctx.lineTo(800, 200);
-ctx.stroke();
-
-ctx.beginPath()
-ctx.rect(400, 400, 100, 100);
-ctx.fillStyle = radialGradient;
+ctx.fillStyle = 'tomato';
+ctx.strokeStyle = 'teal';
+ctx.lineWidth = 5;
+ctx.ellipse(canvas.width/2, canvas.height/2, 240, 150, Math.PI/4, 0, 2*Math.PI, false);
 ctx.fill();
+ctx.stroke();
